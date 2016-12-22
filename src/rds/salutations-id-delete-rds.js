d1923da31f1f9ba1518fc18af7cff21967ffa920
mdang8 <mdang808@gmail.com>
2016-12-22 11:17:51 -0500

    Fixed the RDS DELETE function comments to be more descriptive
    Included the data types of the params

--- src/rds/salutations-id-delete-rds.js
+++ src/rds/salutations-id-delete-rds.js
@@ -10,8 +10,8 @@ let deleteRecordId;
 /**
  * Connects to the database and sets the client.
  *
- * @param context       the context from the exports.handler
- * @param callback      the callback function
+ * @param {Object} context - the context from the exports.handler
+ * @param {function} callback - the callback function
  */
 exports.init = function init(context, callback) {
     initDatabase.connectDB(context, function (results) {
@@ -23,8 +23,8 @@ exports.init = function init(context, callback) {
 /**
  * Gets the id from the event and sets the variable id.
  *
- * @param event         the event from the exports.handler
- * @param callback      the callback function
+ * @param {Object} event - the event from the exports.handler
+ * @param {function} callback - the callback function
  */
 exports.setup = function setup(event, callback) {
     deleteRecordId = event.params.id !== undefined ? event.params.id : '';
@@ -35,8 +35,8 @@ exports.setup = function setup(event, callback) {
 /**
  * Deletes the record with the specified id and passes it to the callback results.
  *
- * @param context       the context from the exports.handler
- * @param callback      the callback function
+ * @param {Object} context - the context from the exports.handler
+ * @param {function} callback - the callback function
  */
 exports.process = function process(context, callback) {
     dataFunctions.getSingleRecord(deleteRecordId, client, context, function (results) {
@@ -49,8 +49,8 @@ exports.process = function process(context, callback) {
 /**
  * Ends the connection with the database client.
  *
- * @param context       the context from the exports.handler
- * @param callback      the callback function
+ * @param {Object} context - the context from the exports.handler
+ * @param {function} callback - the callback function
  */
 exports.takedown = function takedown(context, callback) {
     client.end(function(err) {
